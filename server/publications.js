9b294a8fbbc18f6189190405b8775ada74dd5c3c
Robey Joyce <robey.za@gmail.com>
2015-10-06 12:30:14 -0700

    Added comment notifications

--- server/publications.js
+++ server/publications.js
@@ -6,3 +6,7 @@ Meteor.publish('comments', function(dealId) {
   check(dealId, String);
   return Comments.find({dealId: dealId});
 });
+
+Meteor.publish('notifications', function() {
+  return Notifications.find();
+});
